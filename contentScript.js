3756006eea4479a15b3e7c2b88443a6c5500ba8e
Kenneth Hua <khuarice@gmail.com>
2022-01-23 19:38:05 -0600

    Optimize iteration to not query duplicates

--- contentScript.js
+++ contentScript.js
@@ -1,25 +1,47 @@
+var deletionQueue = new Set()
 
-let removeShortsFromElement = (element) => {
-    var elements = document.querySelectorAll('a#video-title');
+let removeByTitle = element => {
+    console.log('removingByTitle')
+    var elements = element.querySelectorAll('a#video-title');
     for(let i = 0; i <elements.length; i++){
         if(elements[i].title.includes('#shorts')){
-            console.log(elements[i].title)
-            let item = elements[i].closest("ytd-item-section-renderer")
-            item.parentElement.removeChild(item);
+            console.log('Adding to queue:', elements[i].title)
+            deletionQueue.add(elements[i])
             
         }
     }
 }
 
+let removeByLength = element => {
+    var elements = document.querySelectorAll('ytd-thumbnail-overlay-time-status-renderer')
+}
+
+let removeShortsFromElement = (element) => {
+    removeByTitle(element)
+    deletionQueue.forEach(video => {
+        let container = video.closest("ytd-item-section-renderer")
+        container.parentElement.removeChild(container);
+    })
+    deletionQueue.clear()
+}
+
+// Remove shorts that load on page load
 let subBox = document.querySelector('#contents')
-var insertedNodes = []
-var observer = new MutationObserver(function(mutations){
-    mutations.forEach(function(mutation){
-        for ( let i = 0; i < mutation.addedNodes.length; i++){
-            removeShortsFromElement(mutation.addedNodes[i])
+removeShortsFromElement(subBox)
+
+
+
+var videosSet = new Set()
+// Remove shorts that load on scroll
+var observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        for (let i = 0; i < mutation.addedNodes.length; i++) {
+            videosSet.add(mutation.addedNodes[i])
         }
+        videosSet.forEach(video => {
+            removeShortsFromElement(video)
+        })
+        videosSet.clear()
     })
 })
 observer.observe(subBox, {childList: true})
-
-
